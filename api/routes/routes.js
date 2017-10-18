3c7c8d8b660288a02233fa51e74f1d76c64b44e8
sourabh-kamble <sourabhk0510@gmail.com>
2017-10-19 01:42:15 +0530

    adding cache functionality to get request

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var apicache = require("apicache");
+var cache = apicache.middleware
 var GamesController_1 = require("../controllers/games.controller");
 
 var GamesRoutes = (function() {
@@ -13,7 +15,7 @@ var GamesRoutes = (function() {
             var router = express.Router();
             router.route("/players").get(GamesController_1.GamesController.getAllRecords);
             router.route("/generateMatchData").post(GamesController_1.GamesController.generateMatchData);
-            router.route("/getPlayerStats").get(GamesController_1.GamesController.getPlayerStats);
+            router.route("/getPlayerStats").get(cache('1 minutes'), GamesController_1.GamesController.getPlayerStats);
             return router;
         },
         enumerable: true,
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var mongoOp = require("./api/models/player");
 var GamesRoutes_1 = require("./api/routes/routes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ "extended": false }));
 
 app.use('/', new GamesRoutes_1.GamesRoutes().routes);
-
+	
 app.listen(process.env.PORT || 3000);
 console.log("Listening to PORT 3000");
